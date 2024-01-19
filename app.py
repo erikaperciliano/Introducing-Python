a48722c5792609ee7a4474ca6e6dd30936765ed5
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-01-19 17:21:21 +0000

    creates logout route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin, login_user, LoginManager, login_required
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_key_123'
@@ -40,6 +40,11 @@ def login():
       return(jsonify({'message': 'Logged in successfully!'}))
   return jsonify({'menssage': 'Unauthorized. Invalid credentials'}), 401
 
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+  logout_user()
+  return(jsonify({'message': 'Logout successfully!'}))
 
 @app.route('/api/products/add', methods=['POST'])
 @login_required # To make this request you need to be logged in
